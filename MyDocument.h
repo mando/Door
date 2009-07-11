76fff0b18ef920769e2873e14bbb8a0dc923ada2
Mando Escamilla <mando@mando.org>
2009-07-10 23:26:52 -0500

    Initial Commit of Door.

---
+++ MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  Door
+//
+//  Created by Mando Escamilla on 7/10/09.
+//  Copyright __MyCompanyName__ 2009 . All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Door
+//
+//  Created by Mando Escamilla on 7/10/09.
+//  Copyright __MyCompanyName__ 2009 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}
